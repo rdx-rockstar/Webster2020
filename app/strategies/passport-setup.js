e3509ea1d317afa8254ca4f1f7a2258075e6473e
kshitijver <kshitijverma816@gmail.com>
2020-10-06 20:44:48 +0530

    conflicts removed

--- app/strategies/passport-setup.js
+++ app/strategies/passport-setup.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 // const keys = require('./keys');
 
 passport.use(new GoogleStrategy({
-    clientID:'45047464430-5slrtpctfh3anr0t722jbjce19seujnf.apps.googleusercontent.com',
-    clientSecret: 'HX1yNdA1OAJ9xGOJf_FEWNH4',
+    clientID:keys.google.clientID,
+    clientSecret: keys.google.clientSecret,
     callbackURL: '/auth/google/redirect'
 },() => {
 
