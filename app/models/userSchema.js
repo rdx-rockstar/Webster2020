cd6815997d4945a0908c7232c6fcd46702b6a63d
kashish singh <kashishsingh@kashishs-MacBook-Air.local>
2020-10-18 20:50:02 +0400

    added feed +about +contact +followers + following pages

--- app/models/userSchema.js
+++ app/models/userSchema.js
@@ -1,18 +1,17 @@
-
 const mongoose = require('mongoose');
 const passportLocalMongoose= require('passport-local-mongoose');  //for auth for local("passport-local: lib")("passport-local-mongoose:plug-in")
 
 var userSchema = new mongoose.Schema({
     username: {
         type: String,
-        unique: true, 
+        unique: true,
     },
     email: String,
     password: String,
     googleId: String,
     googleProfilePicture: String,
     facebookId: String,
-    provider: String, 
+    provider: String,
     posts:[
         {
             type: mongoose.Schema.Types.ObjectId,
